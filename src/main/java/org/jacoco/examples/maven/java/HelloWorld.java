0da532c0b112a86951f464469c98e975fe67fa12
aforte15 <awojtowi@ryerson.ca>
2017-02-19 23:48:35 -0500

    Update HelloWorld.java

--- src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -3,6 +3,7 @@ package org.jacoco.examples.maven.java;
 public class HelloWorld {
 	
 	public String getMessage(boolean bigger) {
+		blatent_error();
 		if (bigger) {
 			return "Hello Universe!";
 		} else {
